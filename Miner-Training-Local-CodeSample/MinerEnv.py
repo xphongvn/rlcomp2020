95873311e62262df192ef0d4637534e67e505f1f
hungnguyen <39897958+hungadfa@users.noreply.github.com>
2020-07-13 19:22:08 +1000

    Update MinerEnv.py

--- Miner-Training-Local-CodeSample/MinerEnv.py
+++ Miner-Training-Local-CodeSample/MinerEnv.py
@@ -42,7 +42,7 @@ class MinerEnv:
 
     # Functions are customized by client
     def get_state(self):
-        # Building the grid worl
+        # Building the map
         view = np.zeros([self.state.mapInfo.max_x + 1, self.state.mapInfo.max_y + 1], dtype=int)
         for i in range(self.state.mapInfo.max_x + 1):
             for j in range(self.state.mapInfo.max_y + 1):
@@ -50,21 +50,24 @@ class MinerEnv:
                     view[i, j] = -TreeID
                 if self.state.mapInfo.get_obstacle(i, j) == TrapID:  # Trap
                     view[i, j] = -TrapID
-                if self.state.mapInfo.get_obstacle(i, j) == SwampID:  # Swamp
+                if self.state.mapInfo.get_obstacle(i, j) == SwampID: # Swamp
                     view[i, j] = -SwampID
                 if self.state.mapInfo.gold_amount(i, j) > 0:
                     view[i, j] = self.state.mapInfo.gold_amount(i, j)
 
-        DQNState = view.flatten().tolist()
-        # Position of agent and bot
+        DQNState = view.flatten().tolist() #Flattening the map matrix to a vector
+        
+        # Add position and energy of agent to the DQNState
         DQNState.append(self.state.x)
         DQNState.append(self.state.y)
         DQNState.append(self.state.energy)
-
+        #Add position of bots 
         for player in self.state.players:
             if player["playerId"] != self.state.id:
                 DQNState.append(player["posx"])
                 DQNState.append(player["posy"])
+                
+        #Convert the DQNState from list to array for training
         DQNState = np.array(DQNState)
 
         return DQNState
