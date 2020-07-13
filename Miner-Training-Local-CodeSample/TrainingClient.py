5c69ee52b67eccac4fddc3060d3a5fa45c9c6e22
hungnguyen <39897958+hungadfa@users.noreply.github.com>
2020-07-13 19:16:04 +1000

    Update TrainingClient.py

--- Miner-Training-Local-CodeSample/TrainingClient.py
+++ Miner-Training-Local-CodeSample/TrainingClient.py
@@ -38,7 +38,7 @@ DQNAgent = DQN(INPUTNUM, ACTIONNUM)
 memory = Memory(MEMORY_SIZE)
 
 # Initialize environment
-minerEnv = MinerEnv(HOST, PORT) 
+minerEnv = MinerEnv(HOST, PORT) #Creating a communication environment between the DQN model and the game environment (GAME_SOCKET_DUMMY.py)
 minerEnv.start()  # Connect to the game
 
 train = False #The variable is used to indicate that the replay starts, and the epsilon starts decrease.
@@ -52,52 +52,61 @@ for episode_i in range(0, N_EPISODE):
         posID_y = np.random.randint(MAP_MAX_Y) #Choosing a initial position of the DQN agent on Y-axes randomly
         #Creating a request for initializing a map, initial position, the initial energy, and the maximum number of steps of the DQN agent
         request = ("map" + str(mapID) + "," + str(posID_x) + "," + str(posID_y) + ",50,1000") 
-        #
+        #Send the request to the game environment (GAME_SOCKET_DUMMY.py)
         minerEnv.send_map_info(request)
 
         # Getting the initial state
-        minerEnv.reset()
-        s = minerEnv.get_state()
-        total_reward = 0
-        terminate = False
-        maxStep = minerEnv.state.mapInfo.maxStep
+        minerEnv.reset() #Initialize the game environment
+        s = minerEnv.get_state()#Get the state after reseting. 
+                                #This function (get_state()) is an example of creating a state for the DQN model 
+        total_reward = 0 #The amount of rewards for the entire episode
+        terminate = False #The variable indicates that the episode ends
+        maxStep = minerEnv.state.mapInfo.maxStep #Get the maximum number of steps for each episode in training
+        #Start an episde for training
         for step in range(0, maxStep):
-            action = DQNAgent.act(s)  # Getting an action from the model
+            action = DQNAgent.act(s)  # Getting an action from the DQN model from the state (s)
             minerEnv.step(str(action))  # Performing the action in order to obtain the new state
             s_next = minerEnv.get_state()  # Getting a new state
             reward = minerEnv.get_reward()  # Getting a reward
             terminate = minerEnv.check_terminate()  # Checking the end status of the episode
 
-            # Add transition to memory
+            # Add this transition to the memory batch
             memory.push(s, action, reward, terminate, s_next)
 
             # Sample batch memory to train network
             if (memory.length > INITIAL_REPLAY_SIZE):
-                batch = memory.sample(BATCH_SIZE)
-                DQNAgent.replay(batch, BATCH_SIZE)
-                train = True
-            total_reward = total_reward + reward
-            s = s_next
+                #If there are INITIAL_REPLAY_SIZE experiences in the memory batch
+                #then start replaying
+                batch = memory.sample(BATCH_SIZE) #Get a BATCH_SIZE experiences for replaying
+                DQNAgent.replay(batch, BATCH_SIZE)#Do relaying
+                train = True #Indicate the training starts
+            total_reward = total_reward + reward #Plus the reward to the total rewad of the episode
+            s = s_next #Assign the next state for the next step.
 
             # Saving data to file
             save_data = np.hstack(
                 [episode_i + 1, step + 1, reward, total_reward, action, DQNAgent.epsilon, terminate]).reshape(1, 7)
             with open(filename, 'a') as f:
                 pd.DataFrame(save_data).to_csv(f, encoding='utf-8', index=False, header=False)
+            
             if terminate == True:
+                #If the episode ends, then go to the next episode
                 break
 
         # Iteration to save the network architecture and weights
         if (np.mod(episode_i + 1, SAVE_NETWORK) == 0 and train == True):
             DQNAgent.target_train()  # Replace the learning weights for target model with soft replacement
-            now = datetime.datetime.now()
+            #Save the DQN model
+            now = datetime.datetime.now() #Get the latest datetime
             DQNAgent.save_model("TrainedModels/",
                                 "DQNmodel_" + now.strftime("%Y%m%d-%H%M") + "_ep" + str(episode_i + 1))
 
-            # update epsilon
-        print(
-            'Episode %d ends. Number of steps is: %d. Accumlated Reward = %.2f. Epsilon = %.2f .Termination code: %d' % (
+        
+        #Print the training information after the episode
+        print('Episode %d ends. Number of steps is: %d. Accumlated Reward = %.2f. Epsilon = %.2f .Termination code: %d' % (
             episode_i + 1, step + 1, total_reward, DQNAgent.epsilon, terminate))
+        
+        #Decreasing the epsilon if the replay starts
         if train == True:
             DQNAgent.update_epsilon()
 
